d4ac8ce7fe5df0f9f7c04295b38c310f7c0f6099
isdaviddong@home <isdaviddong@hotmail.com>
2019-01-23 16:01:35 +0800

    v1

---
+++ Demo-CognitiveServices/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Demo_CognitiveServices
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.Location = new System.Drawing.Point(14, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(522, 36);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(540, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "選擇";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(10, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "圖檔:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(14, 72);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(239, 189);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox2.Location = new System.Drawing.Point(369, 72);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(239, 189);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.Location = new System.Drawing.Point(259, 222);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 39);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "辨識";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox2.Location = new System.Drawing.Point(19, 283);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(589, 123);
+            this.textBox2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(627, 418);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Demo-CognitiveServices/Form1.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.ProjectOxford.Vision;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo_CognitiveServices
+{
+    public partial class Form1 : Form
+    {
+        string ComputerVisionServiceKey = "f3wfqx2fdad124129sas0af96feac7a"; //更換成你自己的 Key
+        string ComputerVisionServiceEndpoint = "https://southeastasia.api.cognitive.microsoft.com/vision/v1.0";  //更換成你的endpoint
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog d = new OpenFileDialog();
+            d.Filter = "*.png|*.png|*.jpg|*.jpg";
+            if (d.ShowDialog() == DialogResult.OK)
+                this.textBox1.Text = d.FileName;
+
+            this.pictureBox1.Image = Image.FromFile(this.textBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var msg = "";
+
+            //取得圖片檔案FileStream
+            byte[] file = System.IO.File.ReadAllBytes(this.textBox1.Text);
+            Stream MemStream1 = new MemoryStream(file);
+            Stream MemStream2 = new MemoryStream(file);
+            //繪圖用
+            System.Drawing.Bitmap bmp = new System.Drawing.Bitmap(MemStream2);
+            Graphics g = Graphics.FromImage(bmp);
+            //ComputerVision instance
+            var visionClient = new Microsoft.ProjectOxford.Vision.VisionServiceClient(
+               ComputerVisionServiceKey, ComputerVisionServiceEndpoint);
+            //分析用
+            using (MemStream1)
+            {
+                //分析圖片
+                var Results = visionClient.AnalyzeImageAsync(
+                    MemStream1, new VisualFeature[] { VisualFeature.Faces, VisualFeature.Description }).Result;
+                //分別保存性別數量
+                int isM = 0, isF = 0;
+                //如果找到臉，就畫方框標示出來
+                foreach (var item in Results.Faces)
+                {
+                    var faceRect = item.FaceRectangle;
+                    //畫框
+                    g.DrawRectangle(
+                                new Pen(Brushes.Red, 3),
+                                new Rectangle(faceRect.Left, faceRect.Top,
+                                    faceRect.Width, faceRect.Height));
+                    //在方框旁邊顯示年紀
+                    var age = 0;
+                    if (item.Gender.StartsWith("F")) age = item.Age - 2; else age = item.Age;
+                    //劃出數字
+                    g.DrawString(age.ToString(), new Font(SystemFonts.DefaultFont.FontFamily, 30, FontStyle.Bold),
+                        new SolidBrush(Color.Yellow),
+                        faceRect.Left + 3, faceRect.Top + 3);
+                    //紀錄性別數量
+                    if (item.Gender.StartsWith("M"))
+                        isM += 1;
+                    else
+                        isF += 1;
+                }
+                //圖片分析結果
+                msg += $"\n\r圖片說明：\n\r{Results.Description.Captions[0].Text}";
+
+                //如果update了照片，則顯示新圖
+                if (Results.Faces.Count() > 0)
+                {
+                    msg += String.Format("\n\r找到{0}張臉, \n\r{1}男 {2}女", Results.Faces.Count(), isM, isF);
+                }
+            }
+            this.textBox2.Text = msg;
+            this.pictureBox2.Image = bmp;
+        }
+
+
+    }
+}
